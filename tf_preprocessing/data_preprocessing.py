238ac791655ca6113d212bed444d0a02aec20aa5
lau-est <lestacio@unsa.edu.pe>
2022-03-14 21:29:45 +0100

    Some modifications

--- tf_preprocessing/data_preprocessing.py
+++ tf_preprocessing/data_preprocessing.py
@@ -1,13 +1,17 @@
 import os
-import numpy   as     np
-import pandas  as     pd
-from   tqdm    import tqdm
-from   pydicom import dcmread
-from SimpleITK import WriteImage
-from SimpleITK import ClampImageFilter
-from SimpleITK import GetImageFromArray
-from inference_localizer import *
+import csv
+import SimpleITK
+import numpy               as     np
+import pandas              as     pd
+import SimpleITK           as     sitk
+from   tqdm                import tqdm
+from   pydicom             import dcmread
+from   SimpleITK           import WriteImage
+from   SimpleITK           import ClampImageFilter
+from   SimpleITK           import GetImageFromArray
+from   inference_localizer import *
 
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 def read(filename):
     image = dcmread(filename)
@@ -50,7 +54,7 @@ def get_data_file(filename: str, path_to_select: str):
            tcia.dicom_path.apply(lambda x: (path_to_select in x)) &
            tcia.apply(lambda x: safe_difference(x.first_axial_coord, x.second_axial_coord) >= 0.1, axis=1) &
            tcia.apply(lambda x: safe_difference(x.first_axial_coord, x.second_axial_coord) <= 5.0, axis=1) &
-           tcia.number_of_slices.apply(lambda x: x >= 50), :] # Create a variable with slices number and fold name
+           tcia.number_of_slices.apply(lambda x: x >= 50), :]
 
     return tcia
 
@@ -65,7 +69,6 @@ def verify_path_to_save(path: str):
 
 def save_process_tcia_file(tcia, path_to_save, proc_file_name):
     tcia.to_csv(path_to_save + proc_file_name)
-    print("My end shape: ", len(tcia))
     print("The file was processed and saved!")
 
 
@@ -82,49 +85,58 @@ def apply_localizer(tcia_proc: str, path_to_save: str, root_path=''):
         Resample(2),
         PadAndCropTo((192, 192, 160), cval=-1000),
         TransformFromITKFilter(clamp),
-        ZeroOneScaling(),
-        ToNumpyArray(add_batch_dim=False, add_singleton_dim=False)
     )
 
+    f      = open('unprocessed_cts.csv', 'w')
+    writer = csv.writer(f)
+    header = ['dicom_path', 'first_axial_coord', 'number_of_slices', 'second_axial_coord', 'patient_id']
+    writer.writerow(header)
 
     # Reading and saving preprocessed data
     with tqdm(total=len(tcia_proc)) as pbar:
         for index, row in tcia.iterrows():
             try:
-                print("This is my path: ", row['image_path'])
-                path = root_path + row['image_path']
-                processed_ct_scan = loader(path)#(row['image_path'])
-                processed_ct_scan = GetImageFromArray(processed_ct_scan)
+                path              = root_path + row['dicom_path']
+                processed_ct_scan = loader(path)
 
-                WriteImage(processed_ct_scan, path_to_save + str(index) + '.nrrd')
+                # The 120 value is to compensate the clamp.SetLowerBound?
+                processed_ct_scan = processed_ct_scan + 120
+                processed_ct_scan = processed_ct_scan / 2
+                processed_ct_scan = SimpleITK.Cast(processed_ct_scan,sitk.sitkUInt8)
+
+                ct_path = path_to_save + str(index) + '_' + str(row['dicom_path'].split('/')[5]) +  '.nrrd'
+                WriteImage(processed_ct_scan, ct_path)
 
             except:
-                print("---------- CT was not loaded! ----------")
+                print("--------------- CT was not loaded! ---------------")
+                writer.writerow(row)
                 pass
             pbar.update(1)
+    f.close()
 
 
-#path_tcia_csv  = "../../Data/tcia_dataset.csv"
-#path_to_select = '/IMMUNOTEAM/CancerImagingArchive_20200421/CT/CT Lymph Nodes/'
-#path_to_save   = '../../../../../DATA/laura/tcia_proc_data/CT_Lymph_Nodes/' #'../../Data/'
-#proc_file_name = 'tcia_ct_lymph_nodes.csv'
+# ------------------------- Save the preprocessed csv file -------------------------
+'''
+path_tcia_csv  = "../../Data/tcia_dataset.csv"
+path_to_select = '/IMMUNOTEAM/CancerImagingArchive_20200421/CT/CT Lymph Nodes/'
+path_to_save   = '../../../../../DATA/laura/tcia_proc_data/CT_Lymph_Nodes/'
+proc_file_name = 'tcia_ct_lymph_nodes.csv'
 
-#tcia = get_data_file(path_tcia_csv, path_to_select)
-#verify_path_to_save(path_to_save)
-#save_process_tcia_file(tcia, path_to_save, proc_file_name)
-#print('Thank you so much dear God :)!')
+tcia = get_data_file(path_tcia_csv, path_to_select)
+verify_path_to_save(path_to_save)
+save_process_tcia_file(tcia, path_to_save, proc_file_name)
+'''
+# ----------------------------------------------------------------------------------
 
 
-
-
-
+# ------------------------ Save the preprocessed nrrd images ------------------------
+'''
 tcia_file_path       = "../../Data/tcia_ct_lymph_nodes.csv"
-path_root_to_save    = '../../temporal/'
+path_root_to_save    = '../../../../../DATA/laura/tcia_proc_data/CT_Lymph_Nodes/'
 path_data_to_process = '../../../../..'
 tcia                 = pd.read_csv(tcia_file_path, index_col=0)
-tcia                 = tcia[0:3]
-
-
-apply_localizer(tcia_proc= tcia, path_to_save= path_root_to_save, root_path=path_data_to_process)
-
+apply_localizer(tcia_proc = tcia, path_to_save = path_root_to_save,
+                            root_path=path_data_to_process)
+'''
+# ----------------------------------------------------------------------------------
 
