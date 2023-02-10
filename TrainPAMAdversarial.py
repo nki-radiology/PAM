3ba36b80e93cca039c1d450add476d8b11468f62
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-02-10 11:27:21 +0100

    corrections

--- TrainPAMAdversarial.py
+++ TrainPAMAdversarial.py
@@ -56,31 +56,40 @@ def cuda_seeds():
 
 
 def model_init():
-    # Network Definitions
+
+    # Number of GPUs available. Use 0 for CPU mode.
+    ngpu = 1
+
+    # Device we want to run on
+    device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+    # Network Definitions to the device
     pam_net = PAMNetwork()
     dis_net = DiscriminatorNetwork()
+    pam_net.to(device)
+    dis_net.to(device)
+
+    # Handle multi-gpu if desired
+    if (device.type == 'cuda') and (ngpu > 1):
+        pam_net = nn.DataParallel(pam_net, list(range(ngpu)))
+        dis_net = nn.DataParallel(dis_net, list(range(ngpu)))
 
     # Init weights for Generator and Discriminator
     pam_net.apply(weights_init)
     dis_net.apply(weights_init)
 
-    # GPU Computation
-    device = torch.device('cuda:0')
-    pam_net.to(device)
-    dis_net.to(device)
-
     return pam_net, dis_net, device
 
 
-def get_loss_functions():
-    # Loss Functions
-    # affine_loss       = AffineLoss(loss_mult=0.1)
-    # deformation_loss  = ElasticLoss(penalty='l2', loss_mult=0.1)
-    # cross_corr_loss   = NCC()
-    disc_loss = nn.BCELoss()
-    fts_loss  = nn.MSELoss()
+def init_loss_functions():
+    
+    disc_loss   = nn.BCELoss()  # discriminator Loss
+    l2_loss     = nn.MSELoss()  # Feature matching loss: fts_loss
+    nn_loss     = Cross_Correlation_Loss() 
+    energy_loss = Energy_Loss()
 
-    return disc_loss, fts_loss
+
+    return disc_loss, l2_loss, nn_loss, energy_loss
 
 
 def get_optimizers(pam_net, dis_net):
@@ -124,11 +133,11 @@ def load_dataloader():
     return train_dataloader, valid_dataloader
 
 
-def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_optimizer,
+def training(pam_net, dis_net, device, disc_loss, l2_loss, nn_loss, energy_loss, pam_optimizer, dis_optimizer,
              train_dataloader, valid_dataloader):
-    print ('Starting the Training :D')
+    print ('Starting training stage!')
 
-    epoch        = 51
+    epoch        = 0
     n_epochs     = 10001
     train_losses = []
     valid_losses = []
@@ -141,7 +150,7 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
     fake_label   = 0.
 
     # wandb Initialization
-    wandb.init(project=args_pam_adv_fts_sit.wb_project_name, entity='lau-est')
+    wandb.init(project='Adversarial-PAM', entity='lau-est')
 
     # Saving model inputs and hyperparameters
     config          = wandb.config
@@ -153,25 +162,11 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
 
 
     for epoch in range(epoch, n_epochs):
-        loss_affine_t     = 0
-        l_at_sim          = 0
-        l_at_reg          = 0
-        loss_deformation_t= 0
-        l_dt_sim          = 0
-        l_dt_reg          = 0
-        l_gen_t           = 0
+       
         loss_pam_train    = 0
         loss_disc_train   = 0
-
-        loss_affine_v     = 0
-        l_av_sim          = 0
-        l_av_reg          = 0
-        loss_deformation_v= 0
-        l_dv_sim          = 0
-        l_dv_reg          = 0
         loss_pam_valid    = 0
         loss_disc_valid   = 0
-        l_gen_v           = 0
 
         pam_net.train()
         dis_net.train()
@@ -194,36 +189,21 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
             # Forward pass generator (1)
             t_0, w_0, t_1, w_1 = pam_net(fixed, moving)
 
-            # Loss measures generator's ability to fool the discriminator
-            real_pred_gen, real_fts_gen  = dis_net(w_1)
-            _, ground_truth_fts          = dis_net(w_0)
-            # Just Binary cross entropy
-            # d_size  = real_pred_gen.shape
-            # label_g = torch.full(d_size, real_label, dtype=torch.float, device=device)
-            # g_loss  = isc_loss(real_pred_gen, label_g)
-            g_loss                       = gamma_value * fts_loss(real_fts_gen, ground_truth_fts)
+            # Compute distance between feature maps
+            _, features_w1 = dis_net(w_1) 
+            _, features_w0 = dis_net(w_0) 
+            generator_adv_loss = l2_loss(features_w1, features_w0)
 
-            # Computing the Generator Loss
-            # Affine network loss
-            sim_af_loss, reg_af_loss = total_loss(fixed, w_0, w_0)
-            total_affine             = sim_af_loss + alpha_value * reg_af_loss
+            # Generator: Affine network loss
+            registration_affine_loss = nn_loss.pearson_correlation(fixed, w_0)
+            penalty_affine_loss      = energy_loss.energy_loss(t_0)
 
-            l_at_sim      += sim_af_loss.item()
-            l_at_reg      += alpha_value * reg_af_loss.item()
-            loss_affine_t += total_affine.item()
-
-            # Deformation network loss
-            sim_df_loss, reg_df_loss = total_loss(fixed, w_1, t_1)
-            total_elastic            = sim_df_loss + beta_value * reg_df_loss
-
-            l_dt_sim           += sim_df_loss.item()
-            l_dt_reg           += beta_value * reg_df_loss.item()
-            loss_deformation_t += total_elastic.item()
-            l_gen_t            += g_loss.item()
-
+            # Generator: Deformation network loss
+            registration_deform_loss = nn_loss.pearson_correlation(fixed, w_1)
+            penalty_deform_loss = energy_loss.energy_loss(t_1)
+            
             # PAM loss
-            #loss            = alpha_value * total_affine + alpha_value * total_elastic + beta_value * g_loss
-            loss = total_affine + total_elastic + g_loss
+            loss = registration_affine_loss + alpha_value * penalty_affine_loss + registration_deform_loss + beta_value * penalty_deform_loss + gamma_value * generator_adv_loss
             loss_pam_train += loss.item()
 
             # one backward pass
@@ -240,14 +220,14 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
             dis_optimizer.zero_grad()
 
             # Measure discriminator's ability to classify real from generated samples
-            real, fts_real = dis_net(w_0.detach())  # (fixed)
-            fake, fts_fake = dis_net(w_1.detach())
+            real, _ = dis_net(w_0.detach())  # why detach?? Because w_0 was called previously
+            fake, _ = dis_net(w_1.detach())
 
             b_size   = real.shape
             label_r  = torch.full(b_size, real_label, dtype=torch.float, device=device)
             label_f  = torch.full(b_size, fake_label, dtype=torch.float, device=device)
 
-            # Calculate loss
+            # Compute loss
             loss_d_real = disc_loss(real, label_r)
             loss_d_fake = disc_loss(fake, label_f)
             loss_d_t    = (loss_d_real + loss_d_fake) * 0.5
@@ -267,13 +247,12 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
 
             # Display in tensorboard
             # ========
-            wandb.log({'Iteration': it_train_counter, 'Train: Similarity Affine loss': sim_af_loss.item(),
-                       'Train: Regression Affine loss': alpha_value * reg_af_loss.item(),
-                       'Train: Affine loss': total_affine.item(),
-                       'Train: Similarity Elastic loss': sim_df_loss.item(),
-                       'Train: Regression Elastic loss': beta_value * reg_df_loss.item(),
-                       'Train: Elastic loss':  total_elastic.item(),
-                       'Train: Generator Adversarial Loss': g_loss.item(),
+            wandb.log({'Iteration': it_train_counter, 
+                       'Train: Similarity Affine loss': registration_affine_loss.item(),
+                       'Train: Penalty Affine loss': alpha_value * penalty_affine_loss.item(),
+                       'Train: Similarity Elastic loss': registration_deform_loss.item(),
+                       'Train: Penalty Elastic loss': beta_value * penalty_deform_loss.item(),
+                       'Train: Generator Adversarial Loss': generator_adv_loss.item(),
                        'Train: Total loss': loss.item(),
                        'Train: Discriminator Loss': loss_d_t.item()})
 
@@ -298,37 +277,24 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
 
 
                 # Loss measures generator's ability to fool the discriminator
-                real_pred_gen, real_pred_fts = dis_net(w_1)
-                _, ground_truth_fts          = dis_net(w_0)
-
-                # Just Binary cross entropy
-                # d_size  = real_pred_gen.shape
-                # label_g = torch.full(d_size, real_label, dtype=torch.float, device=device)
-                # g_loss  = isc_loss(real_pred_gen, label_g)
-                g_loss    = gamma_value * fts_loss(real_pred_fts, ground_truth_fts)
-
+                _, features_w1 = dis_net(w_1) 
+                _, features_w0 = dis_net(w_0) 
+                generator_adv_loss = l2_loss(features_w1, features_w0)
 
                 # Computing the Generator Loss
                 # Affine network loss
-                sim_af_loss, reg_af_loss = total_loss(fixed, w_0, w_0)
-                total_affine             = sim_af_loss + alpha_value * reg_af_loss
+                registration_affine_loss = nn_loss.pearson_correlation(fixed, w_0)
+                penalty_affine_loss      = energy_loss.energy_loss(t_0)
 
-                l_av_sim      += sim_af_loss.item()
-                l_av_reg      += alpha_value * reg_af_loss.item()
-                loss_affine_v += total_affine.item()
 
                 # Deformation network loss
-                sim_df_loss, reg_df_loss = total_loss(fixed, w_1, t_1)
-                total_elastic            = sim_df_loss + beta_value * reg_df_loss
-
-                l_dv_sim            += sim_df_loss.item()
-                l_dv_reg            += beta_value * reg_df_loss.item()
-                l_gen_v             += g_loss.item()
-                loss_deformation_v  += total_elastic.item()
+                registration_deform_loss = nn_loss.pearson_correlation(fixed, w_1)
+                penalty_deform_loss = energy_loss.energy_loss(t_1)
 
 
                 # PAM loss
-                loss      = total_affine + total_elastic + g_loss
+                # total loss
+                loss = registration_affine_loss + alpha_value * penalty_affine_loss + registration_deform_loss + beta_value * penalty_deform_loss + gamma_value * generator_adv_loss
                 loss_pam_valid += loss.item()
 
 
@@ -352,27 +318,20 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
                 # Display in tensorboard
                 # ========
 
-                wandb.log({'iteration': it_valid_counter, 'Valid: Similarity Affine loss': sim_af_loss.item(),
-                           'Valid: Regression Affine loss': alpha_value * reg_af_loss.item(),
-                           'Valid: Affine loss': total_affine.item(),
-                           'Valid: Similarity Elastic loss': sim_df_loss.item(),
-                           'Valid: Regression Elastic loss': beta_value * reg_df_loss.item(),
-                           'Valid: Elastic loss': total_elastic.item(),
-                           'Valid: Generator Adversarial Loss': g_loss.item(),
-                           'Valid: Total loss': loss.item(), 'Valid: Discriminator Loss': loss_d_v.item()})
+                 wandb.log({'Iteration': it_valid_counter, 
+                       'Valid: Similarity Affine loss': registration_affine_loss.item(),
+                       'Valid: Penalty Affine loss': alpha_value * penalty_affine_loss.item(),
+                       'Valid: Similarity Elastic loss': registration_deform_loss.item(),
+                       'Valid: Penalty Elastic loss': beta_value * penalty_deform_loss.item(),
+                       'Valid: Generator Adversarial Loss': generator_adv_loss.item(),
+                       'Valid: Total loss': loss.item(),
+                       'Valid: Discriminator Loss': loss_d_t.item()})
 
                 if not train_flag:
                     it_valid_counter += 1
 
 
         # Compute the loss per epoch
-        l_at_sim           /= len(train_dataloader)
-        l_at_reg           /= len(train_dataloader)
-        loss_affine_t      /= len(train_dataloader)
-        l_dt_sim           /= len(train_dataloader)
-        l_dt_reg           /= len(train_dataloader)
-        loss_deformation_t /= len(train_dataloader)
-        l_gen_t            /= len(train_dataloader)
         loss_pam_train     /= len(train_dataloader)
         loss_disc_train    /= len(train_dataloader)
 
@@ -387,39 +346,12 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
             print('Saving model')
 
         # Compute the loss per epoch
-        l_av_sim           /= len(valid_dataloader)
-        l_av_reg           /= len(valid_dataloader)
-        loss_affine_v      /= len(valid_dataloader)
-        l_dv_sim           /= len(valid_dataloader)
-        l_dv_reg           /= len(valid_dataloader)
-        loss_deformation_v /= len(valid_dataloader)
-        l_gen_v            /= len(valid_dataloader)
         loss_pam_valid     /= len(valid_dataloader)
         loss_disc_valid    /= len(valid_dataloader)
         valid_losses.append(loss_pam_valid)
 
-
-        # Display in tensorboard
-        # ========
-        wandb.log({'epoch': epoch+1, 'Train: Similarity Affine loss by epoch': l_at_sim,
-                   'Train: Regression Affine loss by epoch': l_at_reg,
-                   'Train: Affine loss by epoch': loss_affine_t, 'Train: Similarity Elastic loss by epoch': l_dt_sim,
-                   'Train: Regression Elastic loss by epoch': l_dt_reg,
-                   'Train: Elastic loss by epoch': loss_deformation_t,
-                   'Train: Generator Adversarial Loss by epoch': l_gen_t,
-                   'Train: Total loss by epoch': loss_pam_train,
-                   'Valid: Similarity Affine loss by epoch by epoch': l_av_sim,
-                   'Valid: Regression Affine loss by epoch': l_av_reg, 'Valid: Affine loss by epoch': loss_affine_v,
-                   'Valid: Similarity Elastic loss by epoch': l_dv_sim,
-                   'Valid: Regression Elastic loss by epoch': l_dv_reg,
-                   'Valid: Elastic loss by epoch': loss_deformation_v,
-                   'Valid: Generator Adversarial Loss by epoch': l_gen_v,
-                   'Valid: Total loss by epoch': loss_pam_valid,
-                   'Train: Discriminator Loss by epoch': loss_disc_train,
-                   'Valid: Discriminator Loss by epoch': loss_disc_valid})
-
         # Print the train and validation losses
-        print("Train epoch : {}/{}, loss_PAM = {:.6f},".format(epoch, n_epochs, loss_pam_train)) # epoch + 1, n_epochs
+        print("Train epoch : {}/{}, loss_PAM = {:.6f},".format(epoch, n_epochs, loss_pam_train)) 
         print("Train epoch : {}/{}, loss_Dis = {:.6f},".format(epoch, n_epochs, loss_disc_train))
         print("Valid epoch : {}/{}, loss_PAM = {:.6f},".format(epoch, n_epochs, loss_pam_valid))
         print("Valid epoch : {}/{}, loss_Dis = {:.6f},".format(epoch, n_epochs, loss_disc_valid))
@@ -430,11 +362,11 @@ def training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_o
 def start_training():
     make_directory_to_save_chkp()
     cuda_seeds()
-    pam_net, dis_net, device           = model_init()
-    disc_loss, fts_loss                = get_loss_functions()
-    pam_optimizer, dis_optimizer       = get_optimizers(pam_net, dis_net)
-    train_dataloader, valid_dataloader = load_dataloader()
-    training(pam_net, dis_net, device, disc_loss, fts_loss, pam_optimizer, dis_optimizer,
+    pam_net, dis_net, device                 = model_init()
+    disc_loss, l2_loss, nn_loss, energy_loss = init_loss_functions()
+    pam_optimizer, dis_optimizer             = get_optimizers(pam_net, dis_net)
+    train_dataloader, valid_dataloader       = load_dataloader()
+    training(pam_net, dis_net, device, disc_loss, l2_loss, nn_loss, energy_loss, pam_optimizer, dis_optimizer,
              train_dataloader, valid_dataloader)
 
 
