a46326dae3c1bbe69f6a30b282123bb5cb770796
valeriopu <valepugliese@yahoo.it>
2023-02-10 14:11:51 +0100

    removing stuff

--- libs/frida/io/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import ImageLoader
-from .base import ReadVolume
-from .base import ReadDICOM
--- libs/frida/transforms/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import Transform
-from .base import RandomizedTransform
-from .base import TransformFromITKFilter
-
-from .base import PadAndCropTo
-from .base import Resample
-from .base import ZeroOneScaling
-from .base import ToNumpyArray
