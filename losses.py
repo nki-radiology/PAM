b3a97f0c4350e0694f7909ff09dbf419dcc73c86
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-02-14 12:07:45 +0100

    changes on the loss function

--- losses.py
+++ losses.py
@@ -5,21 +5,9 @@ def reconstruction_loss(x, x_recon):
     return F.mse_loss(x_recon, x)
     
 
-def kl_divergence(mu, logvar):
-    batch_size = mu.size(0)
-    assert batch_size != 0
-    if mu.data.ndimension() == 4:
-        mu = mu.view(mu.size(0), mu.size(1))
-    if logvar.data.ndimension() == 4:
-        logvar = logvar.view(logvar.size(0), logvar.size(1))
-
-    klds = -0.5*(1 + logvar - mu.pow(2) - logvar.exp())
-    total_kld = klds.sum(1).mean(0, True)
-    dimension_wise_kld = klds.mean(0)
-    mean_kld = klds.mean(1).mean(0, True)
-
-    return total_kld, dimension_wise_kld, mean_kld
-
+def kl_divergence(mu, log_var):
+    kl_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
+    return kl_loss
 
 def imq_kernel(X: torch.Tensor,
                Y: torch.Tensor,
