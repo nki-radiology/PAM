82457957ef17593825bef9cbf4505feab507983a
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-02-17 17:09:26 +0100

    corrections in vanilla WAE

--- networks/wasserstein_network.py
+++ networks/wasserstein_network.py
@@ -3,139 +3,20 @@ import torch.nn             as     nn
 import torch.nn.functional  as     F
 from   collections          import OrderedDict
 from   networks.layer                import conv_layer
-from   networks.layer                import conv_up_layer
 from   networks.layer                import conv_gl_avg_pool_layer
+from   networks.network              import Encoder_WAE
+from   networks.network              import Decoder
 from   networks.spatial_transformer  import SpatialTransformer
-
-
-class Encoder(nn.Module):
-    def __init__(self,
-                 input_ch  : int,
-                 data_dim  : int,
-                 latent_dim: int,
-                 filters   : object = [16, 32, 64, 128, 256]
-                 ):
-        super(Encoder, self).__init__()
-        """
-        Inputs:
-            - input_ch   : Number of input channels of the image. For medical images, this parameter usually is 1
-            - latent_dim : Dimensionality of the latent space (Z)
-            - filters    : Number of channels or filters to use in the convolutional convolutional layers
-        """
-        self.input_ch   = input_ch
-        self.data_dim   = data_dim
-        self.filters    = filters
-        self.latent_dim = latent_dim
-        
-        if self.data_dim == 2:
-            self.input_linear = 8 * 8
-        elif self.data_dim == 3:
-            self.input_linear = 8 * 8 * 8
-        else: 
-            NotImplementedError('only supported 2D and 3D data')
-        
-        self.encoder_net =  nn.Sequential(OrderedDict([
-            ('encoder_conv_16'    , conv_layer(self.data_dim)(in_channels=self.input_ch, out_channels=self.filters[0], 
-                                                              kernel_size=3, stride=2, padding=1, bias=False)), 
-            ('encoder_act_fn_16'  , nn.GELU()), # 256 x 256 => 128 x 128
-            
-            ('encoder_conv_32'    , conv_layer(self.data_dim)(in_channels=self.filters[0], out_channels=self.filters[1], 
-                                                              kernel_size=3, stride=2, padding=1, bias=False)),
-            ('encoder_act_fn_32'  , nn.GELU()), # 128 x 128 => 64 x 64
-            
-            ('encoder_conv_64'    , conv_layer(self.data_dim)(in_channels=self.filters[1], out_channels=self.filters[2], 
-                                                              kernel_size=3, stride=2, padding=1, bias=False)),
-            ('encoder_act_fn_64'  , nn.GELU()), # 64 x 64 => 32 x 32
-            
-            ('encoder_conv_128'   , conv_layer(self.data_dim)(in_channels=self.filters[2], out_channels=self.filters[3], 
-                                                              kernel_size=3, stride=2, padding=1, bias=False)),
-            ('encoder_act_fn_128' , nn.GELU()), # 32 x 32 => 16 x 16
-            
-            ('encoder_conv_256'   , conv_layer(self.data_dim)(in_channels=self.filters[3], out_channels=self.filters[4], 
-                                                              kernel_size=3, stride=2, padding=1, bias=False)),
-            ('encoder_act_fn_256' , nn.GELU()), # 16 x 16 => 8 x 8
-            
-            ('encoder_ft_vec_all' , nn.Flatten()),
-            ('encoder_latent_vecZ', nn.Linear(in_features=self.input_linear * self.filters[4], out_features=latent_dim, bias=False)), 
-        ]))
-    
-    def forward(self, x):
-        return self.encoder_net(x)
-    
-    
-    
-class Decoder(nn.Module):
-    def __init__(self,
-                 input_ch  : int,
-                 output_ch : int,
-                 data_dim  : int,
-                 latent_dim: int,
-                 filters   : object = [16, 32, 64, 128, 256]):
-        super(Decoder, self).__init__()
-        """
-        Inputs:
-            - input_ch   : Number of input channels of the image. For medical images, this parameter usually is 1
-            - latent_dim : Dimensionality of the latent space (Z)
-            - filters    : Number of channels or filters to use in the convolutional convolutional layers
-        """          
-        self.input_ch   = input_ch
-        self.output_ch  = output_ch
-        self.data_dim   = data_dim
-        self.filters    = filters
-        self.latent_dim = latent_dim
-        
-        if self.data_dim == 2:
-            self.input_linear = 8 * 8
-        elif self.data_dim == 3:
-            self.input_linear = 8 * 8 * 8
-        else: 
-            NotImplementedError('only supported 2D and 3D data')
-        
-        self.linear = nn.Sequential(OrderedDict([
-            ('decoder_linear'       , nn.Linear(self.latent_dim, self.input_linear * self.filters[4])), 
-            ('decoder_act_fn_linear', nn.GELU()), 
-        ]))
-        
-        self.decoder_net =  nn.Sequential(OrderedDict([
-            ('decoder_conv_256'   , conv_up_layer(self.data_dim)(in_channels=self.filters[4], out_channels=self.filters[3], 
-                                                              kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)), 
-            ('decoder_act_fn_256' , nn.GELU()), # 8 x 8 => 16 x 16
-            
-            ('decoder_conv_128'   , conv_up_layer(self.data_dim)(in_channels=self.filters[3], out_channels=self.filters[2], 
-                                                              kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)),
-            ('decoder_act_fn_128' , nn.GELU()), # 16 x 16 => 32 x 32
-            
-            ('decoder_conv_64'    , conv_up_layer(self.data_dim)(in_channels=self.filters[2], out_channels=self.filters[1], 
-                                                              kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)),
-            ('decoder_act_fn_64'  , nn.GELU()), # 32 x 32 => 64 x 64
-            
-            ('decoder_conv_32'    , conv_up_layer(self.data_dim)(in_channels=self.filters[1], out_channels=self.filters[0], 
-                                                              kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)),
-            ('decoder_act_fn_32'  , nn.GELU()), # 64 x 64 => 128 x 128
-            
-            ('decoder_conv_16'    , conv_up_layer(self.data_dim)(in_channels=self.filters[0], out_channels=self.output_ch, 
-                                                              kernel_size=3, stride=2, padding=1, output_padding=1, bias=False)),
-            ('decoder_act_fn_16'  , nn.Tanh()), # 128 x 128 => 256 x 256   
-        ]))
-    
-    def forward(self, x):
-        x = self.linear(x)
-        if self.data_dim == 2:
-            x = x.reshape(x.shape[0], -1, 8, 8)
-        elif self.data_dim == 3:
-            x = x.reshape(x.shape[0], -1, 8, 8, 8)
-        else: 
-            NotImplementedError('only support 2D and 3D data')
-        x = self.decoder_net(x)
-        return x
-     
 
 
 class Wasserstein_AE(nn.Module):
     def __init__(self, 
-                 z_latent_dim      : int,
-                 num_input_channels: int,
-                 num_outpt_channels: int
+                input_ch  : int = 1,
+                input_dim : int = [256, 256, 512],
+                latent_dim: int = 512,
+                output_ch : int = 3,
+                group_num : int = 8,
+                filters   : object = [32, 64, 128, 256]
                  ):
         super(Wasserstein_AE, self).__init__()
         """
@@ -143,21 +24,27 @@ class Wasserstein_AE(nn.Module):
             - num_input_channels: Number of input channels of the image. For medical images, this parameter usually is 1
             - z_latent_dim      : Dimensionality of the latent space (Z)
         """
-        self.z_latent_dim   = z_latent_dim
-        self.input_channels = num_input_channels
-        self.output_channels= num_outpt_channels
-        self.filters        = [16, 32, 64, 128, 256]
+        self.input_ch   = input_ch
+        self.input_dim  = input_dim
+        self.latent_dim = latent_dim
+        self.output_ch  = output_ch
+        self.group_num  = group_num
+        self.filters    = filters
         
         # Encoder Block
-        self.encoder  = Encoder(self.input_channels, self.z_latent_dim, self.filters)
+        self.encoder  = Encoder_WAE(input_ch=self.input_ch, input_dim=self.input_dim, latent_dim=self.latent_dim,
+                                    group_num=self.group_num, filters=self.filters)
         
         # Decoder Block
-        self.decoder  = Decoder(self.input_channels, self.output_channels, self.z_latent_dim, self.filters)
+        self.decoder  = Decoder(output_ch=self.output_ch, input_dim=self.input_dim, latent_dim=self.latent_dim,
+                                group_num=self.group_num, filters=self.filters)
+        self.activation_func = nn.Tanh()
         
     
     def forward(self, x):
         z     = self.encoder(x)
         x_hat = self.decoder(z)
+        x_hat = self.activation_func(x_hat)
         return x_hat
 
 
