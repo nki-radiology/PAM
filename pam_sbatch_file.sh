e9436af098853bfdeb35beb5bdfdfa9d877b41a7
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-02-26 09:57:01 +0100

    pam sbatch file

---
+++ pam_sbatch_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=pam_thorax                       # Job name
+#SBATCH --partition=p6000                           # Partition
+#SBATCH --gpus-per-task=1                           # Number of gpus per node
+#SBATCH --gpus=1                                    # Number of gpus in total
+#SBATCH --ntasks=1                                  # Run on a single node
+#SBATCH --mem=20G                                   # Memory
+#SBATCH --cpus-per-task=5                           # Number of cores
+#SBATCH --time=96:00:00                             # Time limit hrs:min:sec
+#SBATCH --output=/projects/disentanglement_methods/temp/PAM/outputjobs/slurm_%j.log   # Standard output and error log
+pwd; hostname; date
+
+# Source bashrc, such that the shell is setup properly
+source ~/.bashrc
+# Activate conda environment pyenv
+source /home/l.estacio/miniconda3/bin/activate pytorch
+
+# Load cuda and cudnn (make sure versions match)
+# eval `spack load --sh cuda@11.3 cudnn@8.2.0.53-11.3`
+
+# Run your command
+python /projects/disentanglement_methods/temp/PAM/TrainPAMAdversarial.py
