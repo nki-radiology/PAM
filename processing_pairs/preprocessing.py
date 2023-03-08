2afc034cd0d9073e0c7dac1bf848ed1b60a9a8b1
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-03-08 15:04:48 +0100

    Corrections about date format

--- processing_pairs/preprocessing.py
+++ processing_pairs/preprocessing.py
@@ -37,51 +37,64 @@ class Preprocessing(object):
         # Removing all the patients with less than 2 scans and saving them
         yads_data = yads_data.loc[yads_data.NumberOfDicoms.apply(lambda x: x >= 2)]
         yads_data.to_csv(args.path_to_save_file + '2.YADS_patients_with_more_than_one_scan.csv', na_rep='NULL', index=False, encoding='utf-8')
+        print("End CT scans number: ", len(yads_data))
         return yads_data
     
     
-    def get_difference_days(self, ct1_date, ct2_date):
-        input_date_list = [datetime.strptime(ct1_date, '%Y-%m-%d'), datetime.strptime(ct2_date, '%Y-%m-%d')]
-        #input_date_list.sort(key=lambda date: datetime.strptime(date, "%Y-%m"))
-        difference_days = (input_date_list[1] - input_date_list[0]).days
-        return difference_days
-    
-    
     def get_date(self, directory, idx):
-        directory_date           = datetime.strptime(str(directory[idx])[:8], '%Y%m%d').strftime('%Y-%m-%d')
+        directory_date           = datetime.strptime(str(directory[idx])[:8], '%Y%m%d').strftime('%Y%m%d')
         remaining_directory_name = str(directory[idx])[8:]
         new_directory            = {'date_directory': directory_date, 'remaining_name_directory': remaining_directory_name}
         return new_directory
+
     
+    def get_difference_days(self, ct1_date, ct2_date):
+        input_date_list = [datetime.strptime(ct1_date, '%Y%m%d'), datetime.strptime(ct2_date, '%Y%m%d')]
+        difference_days = (input_date_list[1] - input_date_list[0]).days
+        return difference_days
+           
            
     def get_difference_between_scans(self, yads):
         
         pairs  = []
-            
+
         for i in range(len(yads)):
             directories      = next(os.walk(yads.iloc[i]['PathRawDicom']))[1]
             directories_date = list(map(lambda i:self.get_date(directories, i), range(0, len(directories))))
-            directories_date.sort(key = lambda x: datetime.strptime(x['date_directory'], '%Y-%m-%d'))
+            
             # Get the values
             max_len = len(directories_date) - 1
             for j in range (max_len):
-                idx = max_len - j
-                idx_prev = max_len - j - 1
-                difference_in_days_between_scans = self.get_difference_days(directories_date[idx_prev]["date_directory"], directories_date[idx]["date_directory"])
-                pairs.append([ yads.iloc[i]['AnonymizedName'] , yads.iloc[i]['PatientID'], yads.iloc[i]['YADSId'], yads.iloc[i]['AnonymizedPatientID'], 
-                    yads.iloc[i]['Included'], directories_date[idx_prev]["date_directory"], 
-                    yads.iloc[i]['PathRawDicom'] + directories_date[idx_prev]["date_directory"] + directories_date[idx_prev]["remaining_name_directory"], 
-                    directories_date[idx]["date_directory"],
-                    yads.iloc[i]['PathRawDicom'] + directories_date[idx]["date_directory"] + directories_date[idx]["remaining_name_directory"],
-                    difference_in_days_between_scans])
-                
+                for k in range(j + 1, len(directories_date)):
+                    # Sorting dates
+                    dates = [directories_date[j], directories_date[k]]
+                    dates.sort(key = lambda x: datetime.strptime(x['date_directory'], '%Y%m%d'))
+                    # Difference in days
+                    difference_in_days_between_scans = self.get_difference_days(dates[0]["date_directory"], dates[1]["date_directory"])
+                    pairs.append([ yads.iloc[i]['AnonymizedName'] , yads.iloc[i]['PatientID'], yads.iloc[i]['YADSId'], 
+                                  yads.iloc[i]['AnonymizedPatientID'], yads.iloc[i]['Included'], 
+                                  dates[0]["date_directory"], yads.iloc[i]['PathRawDicom'] + dates[0]["date_directory"] + dates[0]["remaining_name_directory"], 
+                                  dates[1]["date_directory"], yads.iloc[i]['PathRawDicom'] + dates[1]["date_directory"] + dates[1]["remaining_name_directory"],
+                                  difference_in_days_between_scans])
+ 
         yads_including_days = pd.DataFrame(pairs, columns=['AnonymizedName', 'PatientID', 'YADSId', 'AnonymizedPatientID', 'Included', 'PRIOR_DATE', 'PRIOR_PATH', 'SUBSQ_DATE', 'SUBSQ_PATH', 'DifferenceInDaysBetweenScans'])
         yads_including_days.to_csv(args.path_to_save_file + '3.YADS_including_difference_in_days.csv', na_rep='NULL', index=False, encoding='utf-8')
-        
         return yads_including_days
+
     
     def exclude_patients_based_on_number_of_days(self, data):
         included_patients_yads = data.loc[data.DifferenceInDaysBetweenScans.apply(lambda x: x >= 30 and x <= 120)]
+        
+        # Changing to date format and avoiding SettingWithCopyWarning
+        included_patients_yads = included_patients_yads[included_patients_yads['PRIOR_DATE'].notnull()].copy()
+        included_patients_yads = included_patients_yads[included_patients_yads['SUBSQ_DATE'].notnull()].copy()
+        included_patients_yads['PRIOR_DATE'] = included_patients_yads['PRIOR_DATE'].apply(lambda x: datetime.strptime(x, '%Y%m%d'))
+        included_patients_yads['SUBSQ_DATE'] = included_patients_yads['SUBSQ_DATE'].apply(lambda x: datetime.strptime(x, '%Y%m%d'))
+        
+        # Sort by AnonymizedName and PRIOR_DATE
+        included_patients_yads = included_patients_yads.sort_values(by=['AnonymizedName', 'PRIOR_DATE'])
+        
+        # Save dataframe
         included_patients_yads.to_csv(args.path_to_save_file + '4.YADS_with_included_patients.csv', na_rep='NULL', index=False, encoding='utf-8')
         return included_patients_yads
     
