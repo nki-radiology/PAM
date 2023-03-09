b45358b444f95db475c5f7a574d82e617c42bccc
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-03-09 01:13:54 +0100

    Adding date_of_death and date_of_last_check

--- processing_pairs/preprocessing.py
+++ processing_pairs/preprocessing.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import argparse
 from datetime                           import datetime
 
@@ -10,6 +11,7 @@ class Preprocessing(object):
         self.max_days_inclusion  = args.max_days_inclusion
         self.path_to_save_file   = args.path_to_save_file
         self.path_to_cts_in_yads = args.path_to_cts_in_yads
+        self.patients_file_to_read = args.patients_file_to_read
     
     def assign_path(self, entry):
         path = self.path_to_cts_in_yads + entry.AnonymizedName + '/'
@@ -32,11 +34,11 @@ class Preprocessing(object):
         yads_data.insert(len(yads_data.columns), 'NumberOfDicoms', yads_data.apply(self.assign_number_of_dicoms, axis=1))
         
         # Saving as 1.YADS_with_cts_numbers.csv. This is only to check if the number of dicoms is right
-        yads_data.to_csv(args.path_to_save_file + '1.YADS_with_number_of_scans.csv', na_rep='NULL', index=False, encoding='utf-8')
+        yads_data.to_csv(self.path_to_save_file + '1.YADS_with_number_of_scans.csv', na_rep='NULL', index=False, encoding='utf-8')
         
         # Removing all the patients with less than 2 scans and saving them
         yads_data = yads_data.loc[yads_data.NumberOfDicoms.apply(lambda x: x >= 2)]
-        yads_data.to_csv(args.path_to_save_file + '2.YADS_patients_with_more_than_one_scan.csv', na_rep='NULL', index=False, encoding='utf-8')
+        yads_data.to_csv(self.path_to_save_file + '2.YADS_patients_with_more_than_one_scan.csv', na_rep='NULL', index=False, encoding='utf-8')
         print("End CT scans number: ", len(yads_data))
         return yads_data
     
@@ -78,7 +80,7 @@ class Preprocessing(object):
                                   difference_in_days_between_scans])
  
         yads_including_days = pd.DataFrame(pairs, columns=['AnonymizedName', 'PatientID', 'YADSId', 'AnonymizedPatientID', 'Included', 'PRIOR_DATE', 'PRIOR_PATH', 'SUBSQ_DATE', 'SUBSQ_PATH', 'DifferenceInDaysBetweenScans'])
-        yads_including_days.to_csv(args.path_to_save_file + '3.YADS_including_difference_in_days.csv', na_rep='NULL', index=False, encoding='utf-8')
+        yads_including_days.to_csv(self.path_to_save_file + '3.YADS_including_difference_in_days.csv', na_rep='NULL', index=False, encoding='utf-8')
         return yads_including_days
 
     
@@ -98,16 +100,40 @@ class Preprocessing(object):
         included_patients_yads.to_csv(args.path_to_save_file + '4.YADS_with_included_patients.csv', na_rep='NULL', index=False, encoding='utf-8')
         return included_patients_yads
     
+    
+    def read_and_get_included_patients(self, yads_data):
+        
+        patients_data = pd.read_csv(self.patients_file_to_read)
+        patients_data['date_of_death'] = pd.to_datetime(patients_data['date_of_death'], format='%Y-%m-%d', errors='coerce')
+
+        # Rename the patients_data column from avl_id to PatientID to be similar to the column name in yads_data
+        patients_data.rename(columns={'avl_id' : 'PatientID'}, inplace = True)
+        
+        # Selecting date_of_death and date_last_check from the patients file with the same ID as yads_date preprocessed file
+        date_of_death   = patients_data.set_index('PatientID')['date_of_death'].to_dict()
+        date_last_check = patients_data.set_index('PatientID')['date_last_check'].to_dict()
+        yads_data['DateOfDeath']     = yads_data['PatientID'].map(date_of_death)
+        yads_data['DateOfLastCheck'] = yads_data['PatientID'].map(date_last_check)
+        
+        # Saving as 1.YADS_with_cts_numbers.csv. This is only to check if the number of dicoms is right
+        yads_data.to_csv(self.path_to_save_file + '5.YADS_including_date_of_death_and_last_check.csv', na_rep='NULL', index=False, encoding='utf-8')
+        
+        return yads_data
+    
         
 def main(args):
     
     preproc = Preprocessing(args)
     if args.preprocessing_nki:
-        print("Preprocessing ...")
+        print("Preprocessing YADS file... ")
         yads_data = preproc.read_yads_file()
         yads_including_days = preproc.get_difference_between_scans(yads_data)
         included_patients_yads = preproc.exclude_patients_based_on_number_of_days(yads_including_days)
         print('Done :)')
+        
+        print('Preprocessing patient file... ')
+        new_yads_data = preproc.read_and_get_included_patients(included_patients_yads)
+        
     else:
         print('Under development...!')
 
@@ -117,6 +143,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--preprocessing_nki',  default=True, type=bool,  help='Preprocessing or ...')
     parser.add_argument('--YADS_file_to_read',  default='/projects/disentanglement_methods/files_nki/YADSRequestResult.csv', type=str, help='YADS file path')
+    parser.add_argument('--patients_file_to_read', default='/projects/disentanglement_methods/files_nki/patients.csv', type=str, help='Patients file path')
     parser.add_argument('--min_days_inclusion', default=30,   type=int,   help='Minimum days between prior and subsquent CTs')
     parser.add_argument('--max_days_inclusion', default=120,  type=int,   help='Maximum days between prior and subsquent CTs')
     parser.add_argument('--path_to_save_file',  default='/projects/disentanglement_methods/files_nki/', type=str, help='Path to save the preprocessed file of YADS')
