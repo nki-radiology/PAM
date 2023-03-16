949e05a7e7c196af5745e6d74cbc4e4360a8ee61
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2023-03-16 15:41:36 +0100

    minor changes

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_workers',default=8,          type=int,       help='dataloader num_workers')
     parser.add_argument('--start_ep',   default=1,          type=int,       help='start training iteration')
     parser.add_argument('--n_epochs',   default=2000,       type=int,       help='maximum training iteration')
-    parser.add_argument('--batch_size', default=4,          type=int,       help='batch size')
+    parser.add_argument('--batch_size', default=8,          type=int,       help='batch size')
 
     parser.add_argument('--model',      default='Beta-VAE', type=str,       help='Wasserstein Autoencoder (WAE) and Beta Variational Autoencoder (Beta-VAE)')
     parser.add_argument('--add_disc',   default=False,      type=bool,      help='Add a discriminator network to the Beta-VAE model')
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     parser.add_argument('--latent_dim', default=512,        type=int,       help='dimension of the representation z')
     parser.add_argument('--group_num',  default=8,          type=int,       help='Group normalization size')
     parser.add_argument('--input_dim',  default=[192, 192, 304],    type=int,     help='dimension of the data')
-    parser.add_argument('--filters',    default=[4, 8, 16, 32, 64], type=object,  help='filters to create the Beta-VAE')
+    parser.add_argument('--filters',    default=[8, 16, 32, 64], type=object,  help='filters to create the Beta-VAE')
     parser.add_argument('--filters_disc',default=[8, 16, 32, 64, 128, 256, 512], type=object,  help='filters to create the discriminator')
     
     parser.add_argument('--alpha_value', default=0.01,      type=float,     help='beta parameter for the penalty loss')
@@ -56,8 +56,8 @@ if __name__ == '__main__':
     parser.add_argument('--entity_wb',             default='ljestaciocerquin',     type=str, help='Entity for weights and biases')
     
     parser.add_argument('--dset_dir',   default='/data/groups/beets-tan/l.estacio/data_tcia/train/',                    type=str, help='dataset directory')
-    parser.add_argument('--ckpt_dir',   default='/projects/disentanglement_methods/checkpoints/Beta_VAE/bvae/',  type=str, help='checkpoint directory')
-    parser.add_argument('--results_dir',default='/projects/disentanglement_methods/results/Beta_VAE/bvae/',      type=str, help='Results directory of the weight&biases images')
+    parser.add_argument('--ckpt_dir',   default='/projects/disentanglement_methods/checkpoints/Beta_VAE/vanilla/',  type=str, help='checkpoint directory')
+    parser.add_argument('--results_dir',default='/projects/disentanglement_methods/results/Beta_VAE/vanilla/',      type=str, help='Results directory of the weight&biases images')
     
     args = parser.parse_args()
     
