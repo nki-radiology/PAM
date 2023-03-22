1161e511bf39838576020fb07e7dd1eb832061dc
Stefano Trebeschi <stefano@Stefanos-MacBook-Air.local>
2023-03-22 12:16:43 +0100

    sbatch

--- pam_sbatch_file.sh
+++ pam_sbatch_file.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 #SBATCH --time=3-00:00:00                           # Time limit hrs:min:sec
-#SBATCH --job-name=pam_abdomen                      # Job name
+#SBATCH --job-name=split-encoders                   # Job name
 #SBATCH --partition=p6000                           # Partition
 #SBATCH --gpus-per-task=1                           # Number of gpus per node
 #SBATCH --gpus=1                                    # Number of gpus in total
 #SBATCH --ntasks=1                                  # Run on a single node
-#SBATCH --mem=20G                                   # Memory
+#SBATCH --mem=30G                                   # Memory
 #SBATCH --cpus-per-task=5                           # Number of cores
-#SBATCH --output=/projects/disentanglement_methods/temp/PAM/outputjobs_abdomen/slurm_%j.log   # Standard output and error log
+#SBATCH --output=/projects/split-encoders/slurm_%j.log   # Standard output and error log
 pwd; hostname; date
 
 # Source bashrc, such that the shell is setup properly
 source ~/.bashrc
 # Activate conda environment pyenv
-source /home/l.estacio/miniconda3/bin/activate pytorch
+source /home/s.trebeschi/miniconda3/bin/activate pytorch
 
 # Load cuda and cudnn (make sure versions match)
 # eval `spack load --sh cuda@11.3 cudnn@8.2.0.53-11.3`
 
 # Run your command
-python /projects/disentanglement_methods/temp/PAM/TrainPAMAdversarial.py
+python /home/s.trebeschi/PAM/train.py
