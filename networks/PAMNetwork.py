28dc3d759fd244c811e4cb0d8374aaf42ac1f870
Stefano Trebeschi <stefano@Stefanos-MacBook-Air.local>
2023-03-29 17:40:08 +0200

    bugfix

--- networks/PAMNetwork.py
+++ networks/PAMNetwork.py
@@ -4,8 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#from networks.SpatialTransformer import SpatialTransformer 
-from SpatialTransformer import SpatialTransformer # local 
+from networks.SpatialTransformer import SpatialTransformer 
+#from SpatialTransformer import SpatialTransformer # local 
 
 
 """
--- pam_sbatch_file.sh
+++ pam_sbatch_file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=3-00:00:00                           # Time limit hrs:min:sec
+#SBATCH --time=5-00:00:00                           # Time limit hrs:min:sec
 #SBATCH --job-name=split-encoders                   # Job name
 #SBATCH --partition=rtx8000                         # Partition
 #SBATCH --gpus-per-task=1                           # Number of gpus per node
