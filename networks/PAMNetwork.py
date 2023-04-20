b8374266ba44c9bd0051637bc1daca36550330d8
Stefano <stefano.trebeschi.2@gmail.com>
2023-04-20 17:02:27 +0200

    bugfix

--- networks/PAMNetwork.py
+++ networks/PAMNetwork.py
@@ -231,7 +231,9 @@ class PAMNetwork(nn.Module):
     
 
     def forward(self, fixed, moving, z_noise):
-        tA, wA, tD, wD = self.register(fixed, moving)
+        # normal registration path
+        z, (z_fixed, z_moving) = self.encode(fixed, moving)
+        tA, wA, tD, wD = self.decode(z, moving)
         
         # this should be zero
         z_residual, _ = self.encode(fixed, wD)
@@ -241,7 +243,9 @@ class PAMNetwork(nn.Module):
         z_noise_pred, _ = self.encode(fixed, w_noise)
         z_noise_pred = nn.Softmax(dim=1)(z_noise_pred)
 
-        return tA, wA, tD, wD, z_residual, z_noise_pred
+        Z = (z_fixed, z_moving, z_residual, z_noise_pred)
+
+        return tA, wA, tD, wD, Z
 
 
 
