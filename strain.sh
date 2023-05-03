a7c145000953ebeb29a8d14a363a626754cc6806
Stefano Trebeschi <stefano@Stefanos-MacBook-Air.local>
2023-05-03 16:13:26 +0200

    bugfix

--- strain.sh
+++ strain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=7-00:00:00                           # Time limit hrs:min:sec
 #SBATCH --job-name=prj-blue                         # Job name
-#SBATCH --partition=a6000                         # Partition
+#SBATCH --partition=a100                            # Partition
 #SBATCH --gpus-per-task=1                           # Number of gpus per node
 #SBATCH --gpus=1                                    # Number of gpus in total
 #SBATCH --ntasks=1                                  # Run on a single node
