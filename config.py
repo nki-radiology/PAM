f04f21263828f9068b48bca2db023017192b2cf1
Stefano Trebeschi <stefano@Stefanos-MacBook-Air.local>
2023-05-08 13:10:27 +0200

    bugfix

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ parser.add_argument('--img-dim',
 
 parser.add_argument('--batch-size',
                     type = int,
-                    default = 2,
+                    default = 4,
                     help = 'batch size')
 
 parser.add_argument('--filters',
