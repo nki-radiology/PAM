1fbcf3c951b6455c0373099ca1667f1ed87502db
Stefano Trebeschi <stefano@stefano-laptop.local>
2023-05-24 09:31:57 +0200

    bugfix

--- sinference.sh
+++ sinference.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=7-00:00:00                           # Time limit hrs:min:sec
 #SBATCH --job-name=baudolino                        # Job name
-#SBATCH --partition=rtx8000                         # Partition
-#SBATCH --qos=rtx_qos 
+#SBATCH --partition=p6000                         # Partition
+#SBATCH --qos=p6000_qos 
 #SBATCH --gpus-per-task=1                           # Number of gpus per node
 #SBATCH --gpus=1                                    # Number of gpus in total
 #SBATCH --ntasks=1                                  # Run on a single node
-#SBATCH --mem=10G                                   # Memory
+#SBATCH --mem=25G                                   # Memory
 #SBATCH --output=/home/s.trebeschi/log-slurm/slurm_%j.log # Standard output and error log
 pwd; hostname; date
 
