049063b53d2fee4facfd633b76d2c28ea8cb076d
Stefano Trebeschi <stefano@stefano-laptop.local>
2023-05-24 09:34:09 +0200

    bugfix

--- sinference.sh
+++ sinference.sh
@@ -2,7 +2,6 @@
 #SBATCH --time=7-00:00:00                           # Time limit hrs:min:sec
 #SBATCH --job-name=baudolino                        # Job name
 #SBATCH --partition=p6000                         # Partition
-#SBATCH --qos=p6000_qos 
 #SBATCH --gpus-per-task=1                           # Number of gpus per node
 #SBATCH --gpus=1                                    # Number of gpus in total
 #SBATCH --ntasks=1                                  # Run on a single node
