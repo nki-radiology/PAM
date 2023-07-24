0808528f1cff7ef77fdbbec85d8a49e8078793b5
Stefano Trebeschi <stefano@stefano-laptop.local>
2023-07-24 15:52:17 +0200

    refactor

--- scripts/preprocessing/Localizer.py
+++ scripts/preprocessing/Localizer.py
@@ -16,26 +16,16 @@ sys.path.append('../../')
 from   libs.frida.transforms    import Transform
 
 
-class SmartCrop(Transform):
-    def __init__( self, margin=0. ):
-        self.margin = margin
-        self.__load_localizer()
-        super(SmartCrop, self).__init__()
+class LocalizerFactory():
+    def __init__(self):
+        self.__load()
 
-
-    def __load_localizer( self ):
+    def __load( self ):
         self.localizer = load_model(r'localizer.h5', compile=False)
 
+    def init_localizer( self, image ):
+        image_arr = GetArrayFromImage(image)
 
-    def __plot( self, image_arr ):
-        import matplotlib.pyplot as plt
-        # get anatomical coordinates
-        image_arr              = np.clip(image_arr, -120, 300)
-        anatomical_coordinates = self.localizer.predict(image_arr[..., None])
-        plt.plot(anatomical_coordinates)
-
-
-    def __get_coordinates( self, image_arr, query_lo, query_hi ):
         # get anatomical coordinates
         image_arr              = np.clip(image_arr, -120, 300)
         scan_coordinates       = np.arange(image_arr.shape[0])
@@ -46,45 +36,94 @@ class SmartCrop(Transform):
         m.fit(scan_coordinates[..., None], anatomical_coordinates.squeeze())
         coef, intercept = m.estimator_.coef_[0], m.estimator_.intercept_
 
-        # process query
-        #safe = lambda x: min(max(x, 0), image_arr.shape[0])
-        inverse_fn      = lambda x: (x - intercept) / coef
+        return Localizer(coef, intercept, anatomical_coordinates, scan_coordinates)
 
-        margin_fn       = lambda x: max(x, 0) if inverse_fn(query_lo + self.margin) > 0 else -1.
-        query_result_lo = margin_fn((query_lo - intercept) / coef)
 
-        top             = image_arr.shape[0] - 1
-        margin_fn       = lambda x: min(x, top) if inverse_fn(query_hi - self.margin) < top else -1.
-        query_result_hi = margin_fn((query_hi - intercept) / coef)
+class Localizer():
+    def __init__( self, coef, intercept, anatomical_coords, scan_coords ):
+        self.coef                   = coef
+        self.intercept              = intercept
+        self.anatomical_coords      = anatomical_coords
+        self.scan_coords            = scan_coords
 
-        return query_result_lo, query_result_hi
+    def anatomy2scan( self, query ):
+        return (query - self.intercept) / self.coef
+    
+    def scan2anatomy( self, query ):
+        return query * self.coef + self.intercept
+    
+    def get_anatomical_region( self, upper_anatomical_coord, lower_anatomical_coord, tolerance=0):
+        max_scan_coord = np.max(self.scan_coords)
+
+        up = self.anatomy2scan(upper_anatomical_coord)
+        if up > max_scan_coord:
+            if (self.scan2anatomy(max_scan_coord) - upper_anatomical_coord) < tolerance:
+                up = max_scan_coord
+            else:
+                up = -1
+
+        lo = self.anatomy2scan(lower_anatomical_coord)
+        if lo < 0:
+            if (lower_anatomical_coord - self.scan2anatomy(0)) < tolerance:
+                lo = 0
+            else:
+                lo = -1
+
+        return up, lo
+
+
+class SmartCrop(Transform):
+    def __init__( self, tolerance=0. ):
+        self.tolerance = tolerance
+        self.localizer_factory = LocalizerFactory()
+        super(SmartCrop, self).__init__()
 
 
 class CropThorax(SmartCrop):
-    def __init__( self, margin=0. ):
-        super(CropThorax, self).__init__(margin)
+    def __init__( self, tolerance=0. ):
+        super(CropThorax, self).__init__(tolerance)
 
     def __call__(self, image):
         outputs   = None
         image_arr = GetArrayFromImage(image)
-        diaphram, neck = self._SmartCrop__get_coordinates(image_arr, 10, 80) # Initially: (image_arr, 20, 70)
 
-        if (diaphram >= 0) and (neck <= image_arr.shape[0]) and (diaphram < neck):
+        localizer = self.localizer_factory.init_localizer(image_arr)
+        diaphram, neck = localizer.get_anatomical_region(80, 10, self.tolerance)
+
+        if (diaphram >= 0) and (diaphram < neck):
             outputs = Crop(image, [0, 0, int(diaphram)], [0, 0, int(image_arr.shape[0] - neck)])
 
         return outputs
+    
+
+class CropAbdomen(SmartCrop):
+    def __init__( self, tolerance=0. ):
+        super(CropAbdomen, self).__init__(tolerance)
+
+    def __call__(self, image):
+        outputs             = None
+        image_arr           = GetArrayFromImage(image)
+
+        localizer           = self.localizer_factory.init_localizer(image_arr)
+        pelvis, diaphram    = localizer.get_anatomical_region(-70, 25, self.tolerance)
+
+        if (pelvis >= 0) and (pelvis < diaphram):
+            outputs         = Crop(image, [0, 0, int(pelvis)], [0, 0, int(image_arr.shape[0] - diaphram)])
+
+        return outputs
+
 
 class CropAbdomen(SmartCrop):
     def __init__( self, margin=0. ):
         super(CropAbdomen, self).__init__(margin)
 
     def __call__(self, image):
-        outputs          = None
-        image_arr        = GetArrayFromImage(image)
-        pelvis, diaphram = self._SmartCrop__get_coordinates(image_arr, -70, 25)
+        outputs             = None
+        image_arr           = GetArrayFromImage(image)
+        pelvis, diaphram    = self._SmartCrop__get_coordinates(image_arr, -70, 25)
 
         if (pelvis >= 0) and (diaphram <= image_arr.shape[0]) and (pelvis < diaphram):
-            outputs      = Crop(image, [0, 0, int(pelvis)], [0, 0, int(image_arr.shape[0] - diaphram)])
+            outputs         = Crop(image, [0, 0, int(pelvis)], [0, 0, int(image_arr.shape[0] - diaphram)])
 
         return outputs
 
--- scripts/preprocessing/Preprocessing.py
+++ scripts/preprocessing/Preprocessing.py
@@ -51,7 +51,7 @@ cast.SetOutputPixelType(sitk.sitkInt16)
 
 loader = ImageLoader(
     ReadDICOM(),
-    CropObj(margin=25),
+    CropObj(tolerance=25),
     Resample(2),
     PadAndCropTo((192, 192, 160), cval=-1000),
     TransformFromITKFilter(clamp),
@@ -100,11 +100,6 @@ with tqdm(total=len(dcm_folders)) as pbar:
             # check if all files are CT scans
             if not all_files_are_ct_scans(dcm_folder):
                 raise Exception("Not all files are CT scans")
-            
-            #import pdb; pdb.set_trace()
-            #break_loop = False
-            #if break_loop:
-            #    break
 
             # load image
             image = loader(dcm_folder)
